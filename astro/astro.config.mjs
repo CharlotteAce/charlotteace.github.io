aa46b0a5cefcb6608d44928d2e9831b7cb4251d0
CharlotteAce <149445926+CharlotteAce@users.noreply.github.com>
2024-02-01 16:39:38 +0900

    とりあえずページを作った

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    
+    compressHTML: false,
+    outDir: '../docs',
+    site: 'https://chatlotteace.github.io',
+    build: {
+        // Example: Generate `page.html` instead of `page/index.html` during build.
+        format: 'file'
+      }
+    
+});
